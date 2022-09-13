12d771b0b68893d192c5353a60e60536d3fcc00e
ghchoi <ghchoi0427@gmail.com>
2022-09-13 17:35:21 +0900

    feat: 생성자, getter. setter 생성

--- src/main/java/com/dku/dandev/domain/Board.java
+++ src/main/java/com/dku/dandev/domain/Board.java
@@ -1,22 +1,66 @@
 package com.dku.dandev.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Board {
+    @Id
+    @GeneratedValue
     private Long id;
     private Long writerId;
     private String title;
     private String content;
     private int likes;
 
+    public Board() {
+    }
+
+    public Board(Long writerId, String title, String content, int likes) {
+        this.writerId = writerId;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Id
     public Long getId() {
         return id;
+    }
+
+    public Long getWriterId() {
+        return writerId;
+    }
+
+    public void setWriterId(Long writerId) {
+        this.writerId = writerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
     }
 }
