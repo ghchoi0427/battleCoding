5193e26ad91092300a673489804dd6fe7a0a090c
ghchoi <ghchoi0427@gmail.com>
2022-09-13 15:35:14 +0900

    initial commit

---
+++ src/main/java/com/dku/dandev/DandevApplication.java
@@ -0,0 +1,13 @@
+package com.dku.dandev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DandevApplication.class, args);
+    }
+
+}
