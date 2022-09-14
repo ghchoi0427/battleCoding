9af1091b8a99e9bd8faaa03af50b01483f3e8f7b
ghchoi <ghchoi0427@gmail.com>
2022-09-14 23:22:11 +0900

    fix: 필드, getter, setter 추가

--- src/main/java/com/dku/dandev/dto/BoardDto.java
+++ src/main/java/com/dku/dandev/dto/BoardDto.java
@@ -1,4 +1,53 @@
 package com.dku.dandev.dto;
 
+import com.dku.dandev.domain.Board;
+
 public class BoardDto {
+    private Long writerId;
+    private String title;
+    private String content;
+    private int likes;
+
+    public BoardDto(Long writerId, String title, String content, int likes) {
+        this.writerId = writerId;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Long getWriterId() {
+        return writerId;
+    }
+
+    public void setWriterId(Long writerId) {
+        this.writerId = writerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public static BoardDto of(Board board) {
+        return new BoardDto(board.getWriterId(), board.getTitle(), board.getContent(), board.getLikes());
+    }
 }
--- src/main/java/com/dku/dandev/dto/MemberDto.java
+++ src/main/java/com/dku/dandev/dto/MemberDto.java
@@ -1,4 +1,33 @@
 package com.dku.dandev.dto;
 
+import com.dku.dandev.domain.Member;
+
 public class MemberDto {
+    private String name;
+    private String password;
+
+    public MemberDto(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static MemberDto of(Member member) {
+        return new MemberDto(member.getName(), member.getPassword());
+    }
 }
