39f6e20994758d2a48fce3d61e7adc4e175f80e5
ghchoi <ghchoi0427@gmail.com>
2022-09-30 14:07:48 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/com/dku/dandev/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.dku.dandev.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/login")
+public class LoginController {
+
+
+}
