80fea7d8715fee4e43c45c9ac6764e85faed971e
ghchoi <ghchoi0427@gmail.com>
2022-10-12 12:48:13 +0900

    feat: 문제 ID로 테스트케이스 반환 메서드

--- src/main/java/com/dku/dandev/repository/TestcaseRepository.java
+++ src/main/java/com/dku/dandev/repository/TestcaseRepository.java
@@ -4,6 +4,9 @@ import com.dku.dandev.domain.Testcase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TestcaseRepository extends JpaRepository<Testcase, Long> {
+    List<Testcase> findTestcasesByProblemId(Long problemId);
 }
