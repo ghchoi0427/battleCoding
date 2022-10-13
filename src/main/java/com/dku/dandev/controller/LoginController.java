987edf9c18a33ecc8bd014efad6401166884f765
ghchoi <ghchoi0427@gmail.com>
2022-10-13 16:25:48 +0900

    fix: RestController로 변경

--- src/main/java/com/dku/dandev/controller/LoginController.java
+++ src/main/java/com/dku/dandev/controller/LoginController.java
@@ -2,54 +2,61 @@ package com.dku.dandev.controller;
 
 import com.dku.dandev.domain.Member;
 import com.dku.dandev.dto.LoginForm;
-import com.dku.dandev.service.LoginService;
+import com.dku.dandev.dto.MemberDto;
+import com.dku.dandev.service.MemberService;
 import com.dku.dandev.session.SessionConst;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@Controller
-@RequestMapping("/login")
+@RestController
 public class LoginController {
 
-    private final LoginService loginService;
+    private final MemberService memberService;
 
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
+    public LoginController(MemberService memberService) {
+        this.memberService = memberService;
     }
 
-    @PostMapping("")
+    @PostMapping("/login")
     public ResponseEntity<Void> login(@ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
-        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
-
+        Member loginMember = memberService.login(form.getLoginId(), form.getPassword());
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
-
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
-
-        //로그인 성공 처리
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("member/{memberId}")
+    public MemberDto getMemberInfo(@PathVariable Long memberId) {
+        return MemberDto.of(memberService.getMemberById(memberId));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> save(@ModelAttribute MemberDto memberDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        memberService.saveMember(memberDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
