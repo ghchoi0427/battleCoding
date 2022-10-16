e8c10f2f5bb33a16cd793e29dd751578c95a7305
soluinoon <kingjihong@naver.com>
2022-10-16 23:39:30 +0900

    feat: interceptor 추가

---
+++ src/main/java/com/dku/dandev/configuration/WebConfiguration.java
@@ -0,0 +1,39 @@
+package com.dku.dandev.configuration;
+
+import com.dku.dandev.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /*
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/");
+
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns("/signup");
+
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns("/home");
+
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns("/login");
+
+
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/home");
+
+
+       */
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/member");
+
+
+    }
+
+
+}
