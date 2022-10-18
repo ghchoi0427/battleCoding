422255ddea097cf14f384770b828982a6205ae7c
ghchoi <ghchoi0427@gmail.com>
2022-10-19 00:43:30 +0900

    feat: 크롤링, 문제 리스트 반환

--- src/main/java/com/dku/dandev/controller/ProblemController.java
+++ src/main/java/com/dku/dandev/controller/ProblemController.java
@@ -1,18 +1,20 @@
 package com.dku.dandev.controller;
 
 import com.dku.dandev.domain.GradingEntity;
+import com.dku.dandev.domain.Problem;
 import com.dku.dandev.domain.TestStatus;
+import com.dku.dandev.dto.ProblemTestcaseDto;
 import com.dku.dandev.dto.SubmissionDto;
 import com.dku.dandev.service.ProblemService;
+import com.dku.dandev.util.Crawler;
 import com.dku.dandev.util.TestRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/problem")
@@ -33,4 +35,25 @@ public class ProblemController {
         GradingEntity<TestStatus> testResult = testRunner.runTest(code, problemService.findTestcasesByProblemId(problemId));
         return new ResponseEntity<>(testResult, HttpStatus.OK);
     }
+
+    @GetMapping("/crawl")
+    public ResponseEntity<HttpStatus> crawlProblems(@RequestParam(defaultValue = "1000") int start, @RequestParam(defaultValue = "1010") int end) {
+        Crawler crawler = new Crawler(problemService);
+        crawler.crawlProblems(start, end);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Problem>> getProblemsList() {
+        return new ResponseEntity<>(problemService.getAllProblems(), HttpStatus.OK);
+    }
+
+    @GetMapping("/listall")
+    public ResponseEntity<List<ProblemTestcaseDto>> getProblemsAndTestcases() {
+        List<ProblemTestcaseDto> dtos = problemService.getAllProblems()
+                .stream()
+                .map(e -> new ProblemTestcaseDto(e, problemService.findTestcasesByProblemId(e.getId()))).collect(Collectors.toList());
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
 }
