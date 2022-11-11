378a846eedb1cf74a5aa225aba67ad7bf3f882e5
ghchoi <ghchoi0427@gmail.com>
2022-11-11 21:48:49 +0900

    feat: ChatMessage 생성

---
+++ src/main/java/com/dku/dandev/domain/ChatMessage.java
@@ -0,0 +1,31 @@
+package com.dku.dandev.domain;
+
+public class ChatMessage {
+    String sender;
+    String content;
+    MessageType type;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+}
