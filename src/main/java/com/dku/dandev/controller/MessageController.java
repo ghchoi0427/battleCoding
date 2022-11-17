546beb37ffd146eea3fda0c70881e991fa3edbb7
ghchoi <ghchoi0427@gmail.com>
2022-11-17 22:41:18 +0900

    feat: 메시지 컨트롤러 구현

--- src/main/java/com/dku/dandev/controller/MessageController.java
+++ src/main/java/com/dku/dandev/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.dku.dandev.controller;
 
 import com.dku.dandev.domain.ChatMessage;
+import com.dku.dandev.domain.MatchRequestMessage;
+import com.dku.dandev.domain.SystemMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -21,6 +23,18 @@ public class MessageController {
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
+    }
+
+    @MessageMapping("/chat.sendRequest")
+    @SendTo("/topic/public")
+    public MatchRequestMessage sendRequestMessage(@Payload MatchRequestMessage message) {
+        return message;
+    }
+
+    @MessageMapping("/chat.sendData")
+    @SendTo("/topic/public")
+    public SystemMessage sendSystemMessage(@Payload SystemMessage message) {
+        return message;
     }
 
 }
