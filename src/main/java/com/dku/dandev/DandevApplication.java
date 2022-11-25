dfe67283e7e68c157c3ed24723f8f7681bfb7a1c
ghchoi <ghchoi0427@gmail.com>
2022-11-25 22:12:40 +0900

    build: test jenkins

--- src/main/java/com/dku/dandev/DandevApplication.java
+++ src/main/java/com/dku/dandev/DandevApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DandevApplication {
 
     public static void main(String[] args) {
+        //test jenkins
         SpringApplication.run(DandevApplication.class, args);
     }
 
